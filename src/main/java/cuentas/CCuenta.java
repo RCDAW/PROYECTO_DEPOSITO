8231f2317d8fc1925853651193c91cc3c272892f
RCDAW <rolandodj83@gmail.com>
2023-02-12 20:08:08 +0100

    second commit

--- src/main/java/cuentas/CCuenta.java
+++ src/main/java/cuentas/CCuenta.java
@@ -1,96 +1,130 @@
 package cuentas;
 
+/**
+ * @author Cepeda Guaman Rolando
+ *
+ * @version v1 Febrero_2023
+ */
 public class CCuenta {
-
 
     private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInterés;
 
-    public CCuenta()
-    {
+    public CCuenta() {
     }
 
-    public CCuenta(String nom, String cue, double sal, double tipo)
-    {
-        nombre =nom;
-        cuenta=cue;
-        saldo=sal;
+    /*COnstructor por parametro*/
+    /**
+     * @param nom. Nombre del propietario de la cuenta y es una cadena de texto.
+     * @param cue. Numero de cuenta del propietario y sera una cadena de texto.
+     * @param sal. Valor en decimal (double) y sera el saldo que tenemos dentro
+     * de la cuenta.
+     * @param tipo. Valor en decimal (double). 
+     *
+     *
+     */
+    public CCuenta(String nom, String cue, double sal, double tipo) {
+        nombre = nom;
+        cuenta = cue;
+        saldo = sal;
     }
 
-    public double estado()
-    {
+    /*Metodo que permite consultar consultar mi saldo actual*/
+    /**
+     * @return devuelve el saldo de mi cuenta en decimales
+     */
+    public double estado() {
         return getSaldo();
     }
 
-    public void ingresar(double cantidad) throws Exception
-    {
-        if (cantidad<0)
+    /*Metodo que permite ingresar dinero a mi cuenta siempre en cuanto la cantidad que ingrese sea positivo*/
+    /**
+     * @throws Execption. Hace una llamada a Exeption y lanzara la exepcion
+     * cuando ingresemos una cantidad que sea negativo.
+     *
+     * @exception e. Hace una llamada a exepcion "e" y lanzara el error
+     * encontrado.
+     */
+    public void ingresar(double cantidad) throws Exception {
+        if (cantidad < 0) {
             throw new Exception("No se puede ingresar una cantidad negativa");
+        }
         setSaldo(getSaldo() + cantidad);
     }
 
-    public void retirar(double cantidad) throws Exception
-    {
-        if (cantidad <= 0)
-            throw new Exception ("No se puede retirar una cantidad negativa");
-        if (estado()< cantidad)
-            throw new Exception ("No se hay suficiente saldo");
+    /*Metodo que permite retirar dinero a mi cuenta siempre en cuanto la cantidad que ingrese sea positivo y sea menor 
+    a la cantidad que tengamos en la cuenta*/
+    /**
+     * @throws Execption. Hace una llamada a Exeption y lanzara la exepcion
+     * cuando ingresemos una cantidad que sea negativo o que la cantidad que
+     * tengamos en la cuenta sea inferior a lo que vamos a retirar.
+     *
+     * @exception e. Hace una llamada a exepcion "e" y lanzara el error
+     * encontrado.
+     */
+    public void retirar(double cantidad) throws Exception {
+        if (cantidad <= 0) {
+            throw new Exception("No se puede retirar una cantidad negativa");
+        }
+        if (estado() < cantidad) {
+            throw new Exception("No se hay suficiente saldo");
+        }
         setSaldo(getSaldo() - cantidad);
     }
 
     /**
-     * @return the nombre
+     * @return devuelve el nombre del propietario de la cuenta.
      */
     public String getNombre() {
         return nombre;
     }
 
     /**
-     * @param nombre the nombre to set
+     * @param nombres. Es el nombre del propietario de la cuenta
      */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
     /**
-     * @return the cuenta
+     * @return devuelve el numero de la cuenta del propietario.
      */
     public String getCuenta() {
         return cuenta;
     }
 
     /**
-     * @param cuenta the cuenta to set
+     * @param cuenta. Es el numero de cuenta del propiertario
      */
     public void setCuenta(String cuenta) {
         this.cuenta = cuenta;
     }
 
-    /**
-     * @return the saldo
+     /**
+     * @return devuelve el saldo actual de la cuenta.
      */
     public double getSaldo() {
         return saldo;
     }
 
     /**
-     * @param saldo the saldo to set
+     * @param saldo. Es el saldo que tenemos en la cuenta.
      */
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
 
     /**
-     * @return the tipoInterés
+     * @return devuelve el tipo de interes que tiene el propietario.
      */
     public double getTipoInterés() {
         return tipoInterés;
     }
 
     /**
-     * @param tipoInterés the tipoInterés to set
+     * @param tipoInterés. Es el tipo de interes que tiene el propietario.
      */
     public void setTipoInterés(double tipoInterés) {
         this.tipoInterés = tipoInterés;
--- src/main/java/cuentas/Main.java
+++ src/main/java/cuentas/Main.java
@@ -1,8 +1,10 @@
 package cuentas;
 
-
-import cuentas.CCuenta;
-
+/**
+ * @author Cepeda Guaman Rolando
+ *
+ * @version v1 Febrero_2023
+ */
 public class Main {
 
     public static void main(String[] args) {
@@ -13,9 +15,28 @@ public class Main {
         CCuenta cuenta1;
         double saldoActual;
 
-        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        /* Creacion de una cuenta para Antonio Lopez, con numero de cuenta 1000-2365-85-1230456789 que 
+        tendra 2500 como cantida y un 0 como interes*/
+        /**
+         *
+         * @param nom. Nombre del propietario de la cuenta y es una cadena de
+         * texto.
+         * @param cue. Numero de cuenta del propietario y sera una cadena de
+         * texto.
+         * @param sal. Valor en decimal (double) y sera el saldo que tenemos
+         * dentro de la cuenta.
+         * @param tipo. Valor en decimal (double) que sera el tipo de interes 
+         * que tiene el propiertario.
+         */
+        cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
         saldoActual = cuenta1.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
+        System.out.println("El saldo actual es" + saldoActual);
+        
+        /**
+         * @param saldo. valor decimal(double). cuanto dinero queremos retirar o 
+         * agregar al saldo que tenemos.
+         * @exeption e. Hace referencia a la ecepcion de nombre "e" y s elanzara cuando sea requerida
+         */
 
         try {
             cuenta1.retirar(2300);
